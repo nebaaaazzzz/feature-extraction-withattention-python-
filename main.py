8f4ca2bd9a923c32fe6083585eff85c3d4b97a42
nebiyu <nebiyuf@gmail.com>
2025-05-20 16:40:31 +0300

    fixbug : remove distributed computing

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import time
 from torchvision import transforms
 from tiny_imagenet import TinyImageNet
 import utils
-import warnings
 
 def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args):
     model.train()
@@ -89,22 +88,6 @@ def evaluate(model, criterion, data_loader, device, print_freq=100, log_suffix="
             metric_logger.meters["acc5"].update(acc5.item(), n=batch_size)
             num_processed_samples += batch_size
     # gather the stats from all processes
-
-    num_processed_samples = utils.reduce_across_processes(num_processed_samples)
-    if (
-        hasattr(data_loader.dataset, "__len__")
-        and len(data_loader.dataset) != num_processed_samples
-        and torch.distributed.get_rank() == 0
-    ):
-        # See FIXME above
-        warnings.warn(
-            f"It looks like the dataset has {len(data_loader.dataset)} samples, but {num_processed_samples} "
-            "samples were used for the validation, which might bias the results. "
-            "Try adjusting the batch size and / or the world size. "
-            "Setting the world size to 1 is always a safe bet."
-        )
-
-    metric_logger.synchronize_between_processes()
 
     print(f"{header} Acc@1 {metric_logger.acc1.global_avg:.3f} Acc@5 {metric_logger.acc5.global_avg:.3f}")
     return metric_logger.acc1.global_avg
